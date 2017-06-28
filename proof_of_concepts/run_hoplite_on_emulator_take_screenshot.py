e5ae43a0e0e42bfc43b37d06f408a64d1c5bd82a
ebensh <ebensh@gmail.com>
2017-06-28 23:52:40 +0100

    Minor style changes to running hoplite emulator script

--- proof_of_concepts/run_hoplite_on_emulator_take_screenshot.py
+++ proof_of_concepts/run_hoplite_on_emulator_take_screenshot.py
@@ -1,33 +1,23 @@
+#!/usr/bin/env monkeyrunner
+
 import time
 
-# Imports the monkeyrunner modules used by this program
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
-# Connects to the current device, returning a MonkeyDevice object
-device = MonkeyRunner.waitForConnection()
+device = MonkeyRunner.waitForConnection()  # Returns a MonkeyDevice object.
+# device.installPackage('myproject/bin/MyApplication.apk') # Returns a bool.
 
-# Installs the Android package. Returns boolean.
-# device.installPackage('myproject/bin/MyApplication.apk')
-
-# sets a variable with the package's internal name
 package = 'com.magmafortress.hoplite'
-
-# sets a variable with the name of an Activity in the package
 activity = 'com.magmafortress.hoplite.MainActivity'
-
-# sets the name of the component to start
 runComponent = package + '/' + activity
-
-# Runs the component
 device.startActivity(component=runComponent)
 
 # Presses the Menu button
 # device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
 
-time.sleep(5)
 
-# Takes a screenshot
-result = device.takeSnapshot()
 
-# Writes the screenshot to a file
-result.writeToFile('hoplite_screen.png','png')
+time.sleep(3)
+
+result = device.takeSnapshot() # Takes a screenshot
+result.writeToFile('hoplite_screen.png','png') # Writes the screenshot to a file
