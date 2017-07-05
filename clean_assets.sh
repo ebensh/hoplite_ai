cb885bae3a7ede82ad8b43379f029f005de0c79b
ebensh <ebensh@gmail.com>
2017-07-05 16:37:43 +0100

    Cleaning now using auto-trim

--- clean_assets.sh
+++ clean_assets.sh
@@ -1,6 +1,6 @@
 mkdir -p hoplite_assets/cleaned
-for monster in $(ls hoplite_assets/mon*.png); do
-    f=$(basename $monster)
-    convert hoplite_assets/$f +repage -crop 24x24+4+12 -resize 45x45 hoplite_assets/cleaned/$f
+for asset in $(ls hoplite_assets/*.png); do
+    f=$(basename $asset)
+    convert hoplite_assets/$f -trim +repage hoplite_assets/cleaned/$f
 done
 
