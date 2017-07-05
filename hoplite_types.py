fb075c5b5fb66fb34463ed03f5d90ddbede40cef
ebensh <ebensh@gmail.com>
2017-07-05 16:24:49 +0100

    Added printing of Board

--- hoplite_types.py
+++ hoplite_types.py
@@ -2,7 +2,7 @@ from grid_lib import *
 from enum import Enum
 from itertools import groupby
 
-class Monster(Enum):
+class TileType(Enum):
     Bomb = 1
     Bow = 2
     Sword = 3
@@ -15,9 +15,8 @@ ALL_HEXES = [Hex(q, -q - s, s)
              if abs(-q - s) <= 5]
 
 class Board(object):
-  _tiles = []
+  _tiles = {}  # Map from Hex to TileType
 
-  # TODO: This is super hacky, replace with iteration over "y" coordinate.
   def __str__(self):
     row_col_hex = []
     for h in ALL_HEXES:
@@ -25,24 +24,10 @@ class Board(object):
       row_col_hex.append((qoffset.row, qoffset.col, h))
     row_col_hex.sort()
     board_str = ''
-    for _, r_c_hs in groupby(row_col_hex, key=lambda (r, c, h): r):
-      row_str = ' '.join(['1' for r_c_h in r_c_hs])
-      board_str += row_str.center(30) + '\n'
+    for _, r_c_h_iter in groupby(row_col_hex, key=lambda (r, c, h): r):
+      r_c_hs = list(r_c_h_iter)
+      odd_cols_str = ' '.join(['1' for (r, c, h) in r_c_hs if c % 2 == 1])
+      even_cols_str = ' '.join(['0' for (r, c, h) in r_c_hs if c % 2 == 0])
+      if odd_cols_str: board_str += odd_cols_str.center(9) + '\n'
+      if even_cols_str: board_str += even_cols_str.center(9) + '\n'
     return board_str
-    
-      
-    # Any flat layout will do!
-    ret = ''
-    layout = Layout(orientation_flat, Point(50, 50), Point(1000, 1000))
-    def annotate_hex(h):
-      p = hex_to_pixel(layout, h).Round()
-      return (p.y, p.x, h)
-    annotated_hexes = sorted(map(annotate_hex, ALL_HEXES))  # Sorted by y, x, h
-    for y, row_hexes in groupby(annotated_hexes, lambda (y, x, h): y):
-      print row_hexes
-      row_len = sum(1 for _ in row_hexes)
-      row = ' '.join(map(str, range(row_len)))
-      #for h in xrange(len(row_hexes)):
-      #  ret = ret + '1' + ' '
-      ret += row + '\n'
-    return ret
